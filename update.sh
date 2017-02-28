c6cd14e73a4116ed41162f4abfb878fc3aee27c0
Christian Franke <chriss@frankeonline.net>
2017-02-28 22:42:08 +0100

    install deps

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -e
-echo "hallo"
+apt-get update -y
 
-false
+apt-get install -y python3 python3-pip python-virtualenv
+if [ ! -e /venv ]; then
+    /usr/bin/virtualenv /venv --python=/usr/bin/python3
+fi
+source /venv/bin/activate
+pip install PyBarCodeScan --upgrade
+
+/bin/systemctl daemon-reload
+
+/bin/systemctl enable barcodescanner_server.service
+/bin/systemctl restart barcodescanner_server.service
+
+/bin/systemctl enable barcodescanner_reader.service
+/bin/systemctl restart barcodescanner_reader.service
+
