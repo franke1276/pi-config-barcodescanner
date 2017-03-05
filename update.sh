946a3306dba28b9cc02a30f40721251e939b35dd
Christian Franke <chriss@frankeonline.net>
2017-03-05 22:25:10 +0100

    update script

--- update.sh
+++ update.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 set -e
+timedatectl set-timezone "Europe/Berlin"
 apt-get update -y
 
 apt-get install -y python3 python3-pip python-virtualenv redis-server vim
 if [ ! -e /venv ]; then
     /usr/bin/virtualenv /venv --python=/usr/bin/python3
 fi
-source /venv/bin/activate
-pip install PyBarCodeScan --upgrade
-
-/bin/systemctl daemon-reload
-
-/bin/systemctl enable barcodescanner_server.service
-/bin/systemctl restart barcodescanner_server.service
-
-/bin/systemctl enable barcodescanner_reader.service
-/bin/systemctl restart barcodescanner_reader.service
-
 /bin/systemctl enable redis-server.service
 /bin/systemctl restart redis-server.service
+install -m 700 ./update-barcodescanner /usr/sbin/update-barcodescanner
+/usr/sbin/update-barcodescanner
+
