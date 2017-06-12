97fefb1d6897242e5b60f64c685c99601eb50202
Christian Franke <chriss@frankeonline.net>
2017-06-12 21:42:05 +0200

    use node

--- update.sh
+++ update.sh
@@ -3,8 +3,13 @@ set -e
 timedatectl set-timezone "Europe/Berlin"
 apt-get update -y
 
-apt-get install -y vim
-install -m 644 files/barcodescanner.cfg /etc/barcodescanner
 install -m 700 ./update-barcodescanner /usr/sbin/update-barcodescanner
+install -m 700 ./files/pm2-pi.service /etc/systemd/system/pm2-pi.service
+systemctl enable pm2-pi.service
+systemctl start pm2-pi.service
+npm install pm2 -g
+if [ ! -e /home/pi/barcodescanner-js ];then
+    git clone https://github.com/franke1276/barcodescanner-js.git /home/pi/barcodescanner-js
+fi
 /usr/sbin/update-barcodescanner
 
