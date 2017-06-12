ca57f5810a7c6ba8a642a9efa6bb84ee620b4a7a
Christian Franke <chriss@frankeonline.net>
2017-06-12 22:54:37 +0200

    install all automaticly

---
+++ readme.md
@@ -0,0 +1,3 @@
+to run manually:
+
+  $> /usr/sbin/pi-install-local
--- update.sh
+++ update.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 set -e
+
+# set the correct timezone
 timedatectl set-timezone "Europe/Berlin"
+
+# upgrade all packages
 apt-get update -y
 
-install -m 700 ./update-barcodescanner /usr/sbin/update-barcodescanner
-install -m 700 ./files/pm2-pi.service /etc/systemd/system/pm2-pi.service
-systemctl enable pm2-pi.service
-systemctl start pm2-pi.service
-npm install pm2 -g
+# install nodejs
+node_file=node-v6.11.0-linux-armv6l.tar.xz
+node_dir=$(basename $node_file .tar.xz)
+if [ ! -e /usr/lib/$node_dir ];then
+    curl https://nodejs.org/dist/v6.11.0/$node_file -o $node_file
+    tar xvzf $node_fileq -C /usr/lib/
+    cd /usr/bin
+    ln -s /usr/lib/$node_dir/bin/node
+    ln -s /usr/lib/$node_dir/bin/npm
+fi
+
+# install pm2
+if ! npm list pm2 -g ; then
+    npm install pm2 -g
+fi
+
+
+# install update-barcodescanner
 if [ ! -e /home/pi/barcodescanner-js ];then
     git clone https://github.com/franke1276/barcodescanner-js.git /home/pi/barcodescanner-js
 fi
+
+install -m 700 ./update-barcodescanner /usr/sbin/update-barcodescanner
 /usr/sbin/update-barcodescanner
 
+pm2 startup
