92ebf24809b05702114a1d17dbf8faccfee80f2f
Christian Franke <chriss@frankeonline.net>
2017-06-14 23:33:36 +0200

    go back to parent dir

--- update.sh
+++ update.sh
@@ -13,6 +13,7 @@ node_dir=$(basename $node_file .tar.xz)
 if [ ! -e /usr/lib/$node_dir ];then
     curl https://nodejs.org/dist/v6.11.0/$node_file -o $node_file
     tar xJf $node_file -C /usr/lib/
+    pushd .
     cd /usr/bin
     if [ -e /usr/bin/node ]; then
         rm -f /usr/bin/node
@@ -21,6 +22,7 @@ if [ ! -e /usr/lib/$node_dir ];then
     ln -s /usr/lib/$node_dir/bin/node
     ln -s /usr/lib/$node_dir/bin/npm
 fi
+popd
 
 # install pm2
 if ! npm list pm2 -g ; then
